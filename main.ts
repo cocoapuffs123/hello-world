5b05690cb4d853a7b067a3a70bba341d65ae74d8
cocoapuffs123 <122548185+cocoapuffs123@users.noreply.github.com>
2023-01-13 11:26:11 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . # #
+        . # # . .
+        . # # . .
+        . # . # .
+        # . . . #
+        `)
+})
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.TShirt)
+    for (let index = 0; index < 90 * 90; index++) {
+        music.playMelody("B E B F E G E G ", 500)
+    }
 })
